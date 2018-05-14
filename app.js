6113cb059f60fac8d2f1d980357dc9b41f8be3ec
picky123 <38593029+picky123@users.noreply.github.com>
2018-05-14 19:52:57 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = 'NDQ1NTc4NDY0NjI0Mzc3ODg2.Dds7Yw.tBHKa6zCQ-gxyP8dhfohg9MN1k8'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.channel.send(':no_bell:');
+  }
+});
+
+client.login('NDQ1NTc4NDY0NjI0Mzc3ODg2.Dds7Yw.tBHKa6zCQ-gxyP8dhfohg9MN1k8');
+
+client.on('ready', () => {
+  client.user.setGame('ðŸ‘‘ ð˜‰ð˜ˆð˜•ð˜•ð˜Œð˜‹. ', 'https://www.twitch.tv/streamerhouse')
+})
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('444944051544915978').setName(`Member Count: ${member.guild.memberCount}`);
+});
+
+client.on('guildMemberRemove', member => {
+    member.guild.channels.get('444944051544915978').setName(`Member Count: ${member.guild.memberCount}`);
+});
